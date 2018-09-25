3e800d733703190744d99a10d43f51575afff9e3
Alvaro <alvaro.camilloneto@gmail.com>
2018-09-25 00:10:32 -0300

    Tentativa de usar o debounce

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Photo } from './../photo/photo.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-photo-list',
@@ -10,10 +11,12 @@ import { ActivatedRoute } from '@angular/router';
 export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce.subscribe(filter => (this.filter = filter) );
   }
 }
