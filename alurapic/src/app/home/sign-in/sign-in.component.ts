00432925446f681ef610b5ae7d35c47ef63d843d
Alvaro <alvaro.camilloneto@gmail.com>
2018-11-26 23:16:44 -0200

    Criado formulário reativo para o login

--- alurapic/src/app/home/sign-in/sign-in.component.ts
+++ alurapic/src/app/home/sign-in/sign-in.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './sign-in.component.html'
 })
 export class SignInComponent implements OnInit {
-  constructor() {}
+  loginForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this._formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 }
