adecf5cad9447004d5209d279ee3a1be0a5c12cd
Alvaro <alvaro.camilloneto@gmail.com>
2018-11-27 23:17:52 -0200

    Inicio da aplicação dos princípios

--- alurapic/src/app/home/sign-in/sign-in.component.ts
+++ alurapic/src/app/home/sign-in/sign-in.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../core/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
@@ -7,12 +8,22 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SignInComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(
+    private _formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this._formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login() {
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    return this.authService.authenticate(userName, password).subscribe();
   }
 }
