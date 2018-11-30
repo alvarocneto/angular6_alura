f5864fc33603057dab3c21e5757bc85b50c0269e
Alvaro <alvaro.camilloneto@gmail.com>
2018-11-29 23:58:31 -0200

    Proteção para mexer no DOM

--- alurapic/src/app/home/sign-in/sign-in.component.ts
+++ alurapic/src/app/home/sign-in/sign-in.component.ts
@@ -2,7 +2,8 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { AuthService } from './../../core/auth.service';
+import { AuthService } from '../../core/auth/auth.service';
+import { PlatformDetectorService } from './../../core/platform-detector/platform-detector.service';
 
 @Component({
   templateUrl: './sign-in.component.html'
@@ -14,7 +15,8 @@ export class SignInComponent implements OnInit {
   constructor(
     private _formBuilder: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private platformDetectorService: PlatformDetectorService
   ) {}
 
   ngOnInit(): void {
@@ -35,7 +37,8 @@ export class SignInComponent implements OnInit {
       (err) => {
         console.log(err);
         this.loginForm.reset();
-        this.userNameInput.nativeElement.focus();
+        this.platformDetectorService.isPlatformBrowser() &&
+          this.userNameInput.nativeElement.focus();
         alert('Invalid username or password');
       }
     );
