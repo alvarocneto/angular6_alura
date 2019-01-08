17292493b0df0edf878ed17ec0036bc7b357874e
Alvaro <alvaro.camilloneto@gmail.com>
2019-01-07 23:12:45 -0200

    Criada funcao de registro

--- alurapic/src/app/home/sign-up/signup.component.ts
+++ alurapic/src/app/home/sign-up/signup.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
@@ -12,7 +15,9 @@ export class SignUpComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private userNotTakenValidatorService: UserNotTakenValidatorService
+    private userNotTakenValidatorService: UserNotTakenValidatorService,
+    private signUpService: SignUpService,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -37,5 +42,15 @@ export class SignUpComponent implements OnInit {
         [Validators.required, Validators.minLength(8), Validators.maxLength(14)]
       ]
     });
+  }
+
+  signUp() {
+    const newUser = this.signupForm.getRawValue() as NewUser;
+    this.signUpService.signup(newUser).subscribe(
+      () => this.router.navigate(['']),
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 }
